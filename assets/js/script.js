8e5b5a0449f09425d88dd5a5190791a651be3429
Hudson Pereira <hudson.byte@gmail.com>
2014-11-09 09:01:56 -0200

    Nicknames are now supported

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
 $(function() {
 	definePanelHeight();
 
+	$('.panel-heading').append('as [' + getCookie('name') + ']');
+
+
 	$(window).on('resize', definePanelHeight);
 
 	var socket = io();
 
-	$('form').submit(function(){
-		socket.emit('chat message', $('#m').val());
+	$('form').submit(function(e){
+		socket.emit('chat message', {
+			user : getCookie('name'),
+			message : $('#m').val()
+		});
+
 		$('#m').val('');
-		return false;
+
+		e.preventDefault();
 	});
 
 	socket.on('chat message', function(response){
-		console.log(response);
 		var li = $('<li class="list-group-item">');
+
+		li.html($('<div>').addClass('user label label-primary').css('display', 'inline-block').html(response.user + ':'));
 
 		if(response.text)
 		{
-			li.html($('<div>').addClass('text').html(response.text));
+			li.append($('<div>').addClass('text').html(response.text));
 		}
 
 		if(response.media)
@@ -37,4 +46,15 @@ function definePanelHeight()
 	$('.panel-body').css('height',
 		$(window).height()  - $('.panel-heading').height() - ($('.panel-footer').height() * 3.5)
 	);
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
 }
--- index.js
+++ index.js
@@ -3,22 +3,49 @@ var app 		= express();
 var http 		= require('http').Server(app);
 var io 			= require('socket.io')(http);
 var validUrl 	= require('valid-url');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000
 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended : true
+}));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
+
+	if(req.cookies.name == 'undefined')
+	{
+		res.redirect('sign');
+	}
+
 	res.sendFile(__dirname + '/index.html');
+
+});
+
+app.get('/sign', function(req, res){
+	res.sendFile(__dirname + '/sign.html');
+});
+
+app.post('/sign', function(req, res){
+	var name = req.param('name');
+
+	res.cookie('name', name);
+	res.redirect('/');
 });
 
 app.use("/assets", express.static(__dirname + '/assets'));
 
 io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
+	socket.on('chat message', function(input){
 
 		var response = {};
 
-		response.text = msg;
+		response.text = input.message;
+		response.user = input.user;
 
-		var splited = msg.split(' ');
+		var splited = input.message.split(' ');
 
 		for (i in splited)
 		{
